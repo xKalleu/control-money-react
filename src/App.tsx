01b4d2ef1b4ffc4d7f21d0fd8ece49ebac23649f
Kalleu Oliveira <xkalleu@gmail.com>
2021-03-16 20:52:06 -0300

    add: create API

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Modal from 'react-modal'
 import { Dashboard } from "./Components/Dashboard";
 import { Header } from "./Components/Header";
+import { NewTransactionModal } from './Components/NewTransactionModal';
 import { GlobalStyle } from "./styles/global";
 
 Modal.setAppElement('#root')
@@ -22,13 +23,10 @@ export function App() {
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard/>
 
-      <Modal 
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar Transação</h2>
-      </Modal>
-
+      />
       <GlobalStyle/>
     </>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freela',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Clear',
+          type: 'withdraw',
+          category: 'Home',
+          amount: 200,
+          createdAt: new Date('2021-02-12 09:00:00'),
+        },
+        {
+          id: 3,
+          title: 'Freela WebSite',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 1200,
+          createdAt: new Date('2021-02-12 09:00:00'),
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
     
     this.get('/transactions', () => {
-      return {
-        id: 1,
-        title: 'Teste',
-        amount: 400,
-        type: 'deposit',
-        category: 'food',
-        date: '05/06/1997'
-      }
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+      return schema.create('transaction', data)
     })
   }
 }) 
